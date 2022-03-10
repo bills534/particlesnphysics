87402da0cb65396bc45e49a920ecdf790bfcb6d6
Bill <bills534@gmail.com>
2022-03-10 17:30:53 -0500

    misc weird additions

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ from particle import Particle
 # defining some colors
 BLACK = (0,0,0)
 WHITE = (255,255,255)
+LASER_RED = (235,5,5)
 
 # inital pygame settings
 WIDTH = 900
 HEIGHT = 900
 FPS = 60
 BASE_START_POINT = [WIDTH // 2, HEIGHT - 20]
-print(BASE_START_POINT)
+
 # initalize the pygame window
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -29,17 +30,19 @@ def particle_off_screen(position):
 
     return False
 
-# list to hold all the active particles
-active_particles = []
-
 
 def main_game_loop():
-    start_angle = 0
+    # list to hold all the active particles
+    active_particles = []
+    firing = False
 
     while True:
+
+        # drawing a line from the base to the cursor for fun
+        target_line = [(BASE_START_POINT[0], BASE_START_POINT[1]), pygame.mouse.get_pos()]
+
         # getting the angle from the base position to the cursor.
         cursor_position_x, cursor_position_y = pygame.mouse.get_pos()
-        # more trigonometry, been literally 20+ years since ive used any of this
         opposite_len = cursor_position_x - BASE_START_POINT[0] # x 
         adjacent_len = BASE_START_POINT[1] - cursor_position_y # y
         angle_to_cursor = math.degrees(math.atan(opposite_len/adjacent_len))
@@ -48,26 +51,34 @@ def main_game_loop():
         for e in events:
             if e.type == pygame.QUIT:
                 quit()
-            # make a new particle every click
+            # When mouse is pressed down, start firing particles
             if e.type == pygame.MOUSEBUTTONDOWN:
-                # spawn a new particle with every click
-                active_particles.append(Particle(BASE_START_POINT[0],BASE_START_POINT[1],angle=angle_to_cursor,velocity=10,color=WHITE,size=5))
+                firing = True
+            # on Mouseup stop firing
+            if e.type == pygame.MOUSEBUTTONUP:
+                firing = False
 
             if e.type == pygame.KEYDOWN:
                 if e.key == pygame.K_ESCAPE:
                     quit()
         
+        # redraw screen to have a clean slate each frame
         screen.fill(BLACK)
 
+        # if gun is active shoot off particles
+        if firing:
+            active_particles.append(Particle(BASE_START_POINT[0],BASE_START_POINT[1],angle=angle_to_cursor,velocity=10,color=WHITE,size=5))
         
+        # handle particles that have left the screen, draw the ones that have not.
         for particle in active_particles:
-            # clear out particles no longer on screen
             if particle_off_screen(particle.current_position):
                 active_particles.remove(particle)
             else:
                 particle.update()
                 particle.draw(screen)
+        
 
+        pygame.draw.line(screen, LASER_RED, target_line[0], target_line[1])
         pygame.display.update()
         clock.tick(FPS)
         pygame.display.set_caption(f'FPS = {round(clock.get_fps())}')
--- particle.py
+++ particle.py
@@ -5,13 +5,13 @@ class Particle:
     def __init__(self, x=0, y=0, angle=0, velocity=0, color=(0,0,0), size=0):
         self.x = x
         self.y = y
-        self.x_velocity = 0
-        self.y_velocity = 0
         self.angle = angle
-        self.velocity = velocity/100
+        self.velocity = velocity
         self.color = color
         self.size = size
-        self.current_position = [0,0]
+        self.current_position = [0,0]  # used to report the current position and remove particles that are outside the window.
+        self.x_velocity = math.sin(math.radians(self.angle)) * self.velocity
+        self.y_velocity = math.cos(math.radians(self.angle)) * self.velocity
 
     def update(self):
         # setting velocity to the x/y
@@ -20,8 +20,8 @@ class Particle:
         self.current_position = [self.x, self.y]
         
         # updating velocity based on math
-        self.x_velocity += math.sin(math.radians(self.angle)) * self.velocity
-        self.y_velocity += math.cos(math.radians(self.angle)) * self.velocity
+        # self.x_velocity += math.sin(math.radians(self.angle)) * self.velocity
+        self.y_velocity -= 9.8/100 # gravity?
 
     def draw(self, screen):
         border_thickness = 2
